3ff65efe280f5f0ee57abe8eeb951e5e643d1a08
Craig Gant <craigkgant@gmail.com>
2020-01-15 20:00:42 -0500

    added file to gitignore

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const burger = require("../models/burger");
+
+// middleware
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// =====================================
